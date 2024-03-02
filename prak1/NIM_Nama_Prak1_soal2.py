0d6039d655eb78560e2a57f4a5b2f33063b8ef29
MegumenZ <118927140+MegumenZ@users.noreply.github.com>
2024-03-02 17:01:55 +0700

    Update NIM_Nama_Prak1_soal2.py

--- prak1/NIM_Nama_Prak1_soal2.py
+++ prak1/NIM_Nama_Prak1_soal2.py
@@ -1,1 +1,18 @@
+pi = 3.14
 
+def luasLingkaran(jari_jari):
+    return pi * jari_jari**2
+
+def kelilingLingkaran(jari_jari):
+    return 2 * pi * jari_jari
+
+jari_jari = int(input("Masukkan jari-jari lingkaran: "))
+
+if jari_jari < 0:
+    print("Jari-jari lingkaran tidak boleh negatif")
+else:
+    luas = luasLingkaran(jari_jari)
+    keliling = kelilingLingkaran(jari_jari)
+
+print(f"Luas lingkaran: {luas}")
+print(f"Keliling lingkaran: {keliling}")
